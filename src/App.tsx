1662c9ee3138cd71e1739e70c55a53d848ef2100
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-06 22:31:51 +0300

    add local storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,43 @@ import "./App.css";
 
 import { Setter } from "./layout/Setter";
 import { Counter } from "./layout/Counter";
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-  
   const [maxValue, setMaxValue] = useState(10);
   const [minValue, setMinValue] = useState(0);
   const [count, setCount] = useState(0);
   const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const newCount = localStorage.getItem("counterValue");
+    if (newCount) {
+      setCount(JSON.parse(newCount));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("counterValue", JSON.stringify(count));
+  }, [count]);
+
   return (
     <div className="main">
-      <Counter count={count} setCount={setCount} minValue={minValue} maxValue={maxValue} error={error}
+      <Counter
+        count={count}
+        setCount={setCount}
+        minValue={minValue}
+        maxValue={maxValue}
+        error={error}
       />
-      <Setter minValue={minValue} maxValue={maxValue} setMaxValue={setMaxValue} setMinValue={setMinValue} error={error} setCount={setCount} setError={setError}/>
+      <Setter
+        minValue={minValue}
+        maxValue={maxValue}
+        setMaxValue={setMaxValue}
+        setMinValue={setMinValue}
+        error={error}
+        setCount={setCount}
+        setError={setError}
+      />
     </div>
   );
 }
