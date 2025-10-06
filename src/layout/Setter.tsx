02158a059e647f3d6859cca69dd942fe94436bd2
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-06 23:02:46 +0300

    fixed error with logic on error

--- src/layout/Setter.tsx
+++ src/layout/Setter.tsx
@@ -24,20 +24,20 @@ export const Setter = ({
 }: setterPropsType) => {
   const maxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const targetValueMax = Number(e.target.value);
+    setMaxValue(targetValueMax);
     if (targetValueMax <= minValue || targetValueMax === minValue) {
       setError(true);
     } else {
       setError(false);
-      setMaxValue(targetValueMax);
     }
   };
   const minValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const targetValueMin = Number(e.target.value);
+    setMinValue(targetValueMin);
     if (targetValueMin < 0 || targetValueMin >= maxValue) {
       setError(true);
     } else {
       setError(false);
-      setMinValue(targetValueMin);
     }
   };
 
@@ -54,12 +54,12 @@ export const Setter = ({
             />
           </label>
         </div>
-        <div className="counter-input">
+        <div className={error ? "counter-input input-warn" : "counter-input"}>
           <label>
             min value:
             <Input
               value={minValue}
-              name="max-value"
+              name="min-value"
               callback={minValueHandler}
             />
           </label>
